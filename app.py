4def1004fc897b84f028b11abf20b960c780fa27
avdhantyagi <avdhantyagi1011@gmail.com>
2023-07-15 12:44:54 +0530

    inputarraychanges

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, redirect, request, url_for
 app = Flask(__name__, static_folder='static')
 
-inp_arr = [0,0]
+inp_arr = {'algo':".", 'class':".", 'file':"."}
 
 @app.route("/", methods = ['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        inp_arr[0] = request.form.get('algo')
-        inp_arr[1] = request.form.get('file')
-        print('Hello '+inp_arr[1]+' with algo: '+inp_arr[0])
+        inp_arr['algo'] = request.form.get('algo')
+        inp_arr['file'] = request.form.get('file')
+        inp_arr['class'] = request.form.get('class')
         return (redirect(url_for('result')))
     else:
         return render_template('home.html')
@@ -16,7 +16,7 @@ def home():
 @app.route("/result", methods = ['GET', 'POST'])
 def result():
     if request.method == 'GET':
-        return("<h1> Results = " + inp_arr[0] + "</h1>")
+        return("<h1>" + " Results : " + " .. " + inp_arr["file"]+' with algo: '+inp_arr["algo"] + ' in class: '+ inp_arr["class"]+ "</h1>" )
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
