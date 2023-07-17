be407279dd7a74c87e2d17a0df85238667af0fa7
avdhantyagi <avdhantyagi1011@gmail.com>
2023-07-17 14:31:35 +0530

    Py to ipynb to better tackle the error

--- moduletest.py
+++ moduletest.py
@@ -2,11 +2,11 @@ import algoPred as ap
 import pandas as pd
 
 
-inp = "A.jpg"
-algo = input("Algo")
-ui_class = input("Class :")
+inp = "/Users/avdhantyagi/Desktop/Bottle.JPEG"
+algo = input("Algo :")
+ui_class = input("Class :s")
 
-index_for_classes = pd.read_csv("index_for_classes.csv")
+index_for_classes = pd.read_csv("./index_for_classes.csv")
 
 start = 0
 end = 0
@@ -16,7 +16,7 @@ for i in range(len(index_for_classes['class'])):
         end = index_for_classes['id'][i+1]
 
 
-data = pd.read_csv("dataset.csv")
+data = pd.read_csv("./dataset.csv")
 
 if algo == "Jaccard":
     arr = ap.pred_jaccard(inp, data, start, end)
