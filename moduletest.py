91f3160c069d6f4bea5840de254801bbef9b23d7
avdhantyagi <avdhantyagi1011@gmail.com>
2023-07-17 10:30:33 +0530

    initial commit for testing fuctioning of module

---
+++ moduletest.py
@@ -0,0 +1,22 @@
+import algoPred as ap
+import pandas as pd
+
+
+inp = "A.jpg"
+algo = input("Algo")
+ui_class = input("Class :")
+
+index_for_classes = pd.read_csv("index_for_classes.csv")
+
+start = 0
+end = 0
+for i in range(len(index_for_classes['class'])):
+    if(ui_class == index_for_classes['class'][i]):
+        start = index_for_classes['id'][i]
+        end = index_for_classes['id'][i+1]
+
+
+data = pd.read_csv("dataset.csv")
+
+if algo == "Jaccard":
+    arr = ap.pred_jaccard(inp, data, start, end)
